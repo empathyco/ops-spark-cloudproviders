65622723445263a6a8518d5be7b3a982144b8545
Ramiro Alvarez <ramiroa@empathybroker.com>
2019-11-07 00:29:16 +0100

    Insert sleep to bootstap-master

--- aws/emr/scripts/bootstrap_master.sh
+++ aws/emr/scripts/bootstrap_master.sh
@@ -6,12 +6,7 @@ sudo emrfs create-metadata -r 50 -w 10
 
 export AWS_DEFAULT_REGION=eu-west-1
 
+sleep 120
 echo "Launch StepFunction"
-aws stepfunctions start-execution --state-machine-arn arn:aws:states:eu-west-1:XXXXXXXXXXX:stateMachine:devfest --input '{"message": {"message": "DevFest Issue"}}'
+aws stepfunctions start-execution --state-machine-arn arn:aws:states:eu-west-1:323361102328:stateMachine:devfest --input '{"message": {"message": "DevFest Issue"}}'
 
-aws stepfunctions start-execution --state-machine-arn arn:aws:states:eu-west-1:XXXXXXXXXXX:stateMachine:devfest --input "{\"message\": {\"message\": \"DevFestIssue\"}}"
-
-INPUT='{"message": {"message": "DevFest Issue"}}'
-echo "$INPUT" >> /tmp/input.json
-
-aws stepfunctions start-execution --state-machine-arn arn:aws:states:eu-west-1:XXXXXXXXXXX:stateMachine:devfest --input /tmp/input.json
